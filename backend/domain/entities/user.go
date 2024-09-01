7d93eb3303253adc07e8d2499af39e3ba986a3e3
lazaro_barros <lazarobs4@gmail.com>
2024-09-01 18:27:41 -0300

    chore: criando entidade user

---
+++ backend/domain/entities/user.go
@@ -0,0 +1,68 @@
+package entities
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	uuid         string
+	name         string
+	email        string
+	hashPassword string
+}
+
+func NewUser(
+	name string,
+	email string,
+	password string,
+) (obj *User, err error) {
+	if err := validatePassword(password); err != nil {
+		return nil, err
+	}
+	obj = &User{
+		uuid:         uuid.New().String(),
+		name:         name,
+		email:        email,
+		hashPassword: hashPassword(password),
+	}
+	return
+}
+
+func (u *User) Uuid() string {
+	return u.uuid
+}
+
+func (u *User) Name() string {
+	return u.name
+}
+
+func (u *User) Email() string {
+	return u.email
+}
+
+func (u *User) MatchPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.hashPassword), []byte(password))
+	return err == nil
+}
+
+func validatePassword(password string) error {
+	if len(password) < 8 {
+		return fmt.Errorf("a senha deve ter pelo menos 8 caracteres")
+	}
+	// Add other validations here
+	return nil
+}
+
+// HashPassword gera um hash de uma senha usando bcrypt
+func hashPassword(password string) string {
+	// Gera o hash da senha, o segundo parâmetro é o custo do algoritmo (valor comum é 14)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(hashedPassword)
+}
---
+++ backend/domain/entities/user_test.go
@@ -0,0 +1,37 @@
+package entities_test
+
+import (
+	"testing"
+
+	"github.com/Lazaro-Barros/buteco/domain/entities"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUser(t *testing.T) {
+	t.Run("Test Create user and match password", func(t *testing.T) {
+		usr, err := entities.NewUser(
+			"John Doe",
+			"test@test.com",
+			"12345678",
+		)
+		assert.NoError(t, err)
+		assert.NotNil(t, usr)
+		assert.NotEmpty(t, usr.Uuid())
+		assert.Equal(t, "John Doe", usr.Name())
+		assert.Equal(t, "test@test.com", usr.Email())
+
+		assert.True(t, usr.MatchPassword("12345678"))
+	})
+
+	t.Run("Test Create user with invalid password", func(t *testing.T) {
+		usr, err := entities.NewUser(
+			"John Doe",
+			"test@test.com",
+			"showrtP",
+		)
+		assert.Error(t, err)
+		assert.Equal(t, "a senha deve ter pelo menos 8 caracteres", err.Error())
+		assert.Nil(t, usr)
+	})
+
+}
