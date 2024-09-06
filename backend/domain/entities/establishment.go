d96848f61cddaa424b0b14bbc292911fd858fa86
Iury Nogueira <iunog@hotmail.com>
2024-09-06 20:01:26 -0300

    feat: add establishment entity :sparkles:

--- backend/domain/entities/establishment.go
+++ backend/domain/entities/establishment.go
@@ -1,1 +1,38 @@
 package entities
+
+import "github.com/google/uuid"
+
+type Establishment struct {
+	uuid		string
+	name		string
+	address	string
+}
+
+func NewEstablishment(
+	name string,
+	address string,
+) (obj *Establishment, err error) {
+	
+	obj = &Establishment{
+		uuid:  	 uuid.New().String(),
+		name: 	 name,
+		address: address,
+	}
+	return
+}
+
+type IEstablishmentRepository interface {
+	Save(establishment *Establishment) error
+}
+
+func (obj Establishment) Uuid() string {
+	return obj.uuid
+}
+
+func (obj Establishment) Name() string {
+	return obj.name
+}
+
+func (obj Establishment) Address() string {
+	return obj.address
+}
