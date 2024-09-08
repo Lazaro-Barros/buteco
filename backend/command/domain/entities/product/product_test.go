453703c6fb1c6926d63edd82c96afd73c6930432
lazaro_barros <lazarobs4@gmail.com>
2024-09-08 12:09:30 -0300

    test: inserindo teste de product

--- backend/command/domain/entities/product/product_test.go
+++ backend/command/domain/entities/product/product_test.go
@@ -3,13 +3,14 @@ package entities_test
 import (
 	"testing"
 
-	productEntity "github.com/Lazaro-Barros/buteco/command/domain/entities/product"
+	entities "github.com/Lazaro-Barros/buteco/command/domain/entities/product"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestProduct(t *testing.T) {
 	t.Run("Test Create product", func(t *testing.T) {
-		product, err := productEntity.NewProduct(
+		product, err := entities.NewProduct(
 			"Café Moscão",
 			"Café especial em grãos torrados 250g",
 			3500,
@@ -22,5 +23,41 @@ func TestProduct(t *testing.T) {
 		assert.Equal(t, "Café especial em grãos torrados 250g", product.Description())
 		assert.Equal(t, 3500, product.Value())
 		assert.Equal(t, true, product.Visible())
+	})
+
+	t.Run("Test Create product with invalid value", func(t *testing.T) {
+		product, err := entities.NewProduct(
+			"Café Moscão",
+			"Café especial em grãos torrados 250g",
+			0,
+			true,
+		)
+		assert.Error(t, err)
+		assert.Equal(t, entities.ErrInvalidValue.Error(), err.Error())
+		assert.Nil(t, product)
+	})
+
+	t.Run("Test Create product with invalid name", func(t *testing.T) {
+		product, err := entities.NewProduct(
+			"",
+			"Café especial em grãos torrados 250g",
+			3500,
+			true,
+		)
+		assert.Error(t, err)
+		assert.Equal(t, entities.ErrInvalidName.Error(), err.Error())
+		assert.Nil(t, product)
+	})
+
+	t.Run("Test Create product with invalid description", func(t *testing.T) {
+		product, err := entities.NewProduct(
+			"Café Moscão",
+			"",
+			3500,
+			true,
+		)
+		assert.Error(t, err)
+		assert.Equal(t, entities.ErrInvalidDescription.Error(), err.Error())
+		assert.Nil(t, product)
 	})
 }
